a100fd34ea41a4e51ec25c6bd31772edd4032a91
Sergei Klimovich <sergei.kl@firebirdtours.com>
2024-04-13 13:26:41 +0300

    Two_Sum_1

---
+++ MyLeetCode/src/main/java/com/klimovich/arraysandhashing/TwoSum_1.java
@@ -0,0 +1,35 @@
+package com.klimovich.arraysandhashing;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+https://leetcode.com/problems/two-sum/description/
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+================================================================
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+public class TwoSum_1 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
