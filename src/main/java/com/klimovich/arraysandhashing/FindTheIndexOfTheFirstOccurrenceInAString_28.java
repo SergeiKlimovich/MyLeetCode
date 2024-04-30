05261879120f68fc39b9448ac50b05075b2224d1
Sergei Klimovich <sergei.kl@firebirdtours.com>
2024-04-30 23:16:39 +0300

    Find_the_Index_of_the_First_Occurrence_in_a_String_28

---
+++ src/main/java/com/klimovich/arraysandhashing/FindTheIndexOfTheFirstOccurrenceInAString_28.java
@@ -0,0 +1,28 @@
+package com.klimovich.arraysandhashing;
+
+/*
+https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+===========================================
+Example 1:
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+ */
+public class FindTheIndexOfTheFirstOccurrenceInAString_28 {
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        System.out.println(strStr(haystack, needle));
+    }
+
+    public static int strStr(String haystack, String needle) {
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.startsWith(needle, i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
