f5afb9b946d24e5ac1ff583159ee49c0498b72f3
Sergei Klimovich <sergei.kl@firebirdtours.com>
2025-03-22 14:38:29 +0300

    Greatest_Common_Divisor_of_Strings_1071

---
+++ src/main/java/com/klimovich/strings/GreatestCommonDivisorOfStrings_1071.java
@@ -0,0 +1,35 @@
+package com.klimovich.strings;
+
+/*
+https://leetcode.com/problems/greatest-common-divisor-of-strings/description/
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+========================================
+Example 1:
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+========================================
+Example 2:
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+ */
+public class GreatestCommonDivisorOfStrings_1071 {
+    public static void main(String[] args) {
+        String str1 = "ABABAB";
+        String str2 = "ABAB";
+        System.out.println(gcdOfStrings(str1, str2));
+    }
+
+    public static String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+}
